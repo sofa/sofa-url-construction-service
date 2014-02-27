4acc2743960792ef6fad05e6b2e5a390f299be93
Pascal Precht <pascal.precht@googlemail.com>
2014-02-27 17:45:31 +0100

    chore(*): extracts url constructor service

---
+++ dist/sofa.urlConstructionService.js
@@ -0,0 +1,127 @@
+;(function (sofa, undefined) {
+
+'use strict';
+/* global sofa */
+/**
+ * @name UrlConstructionService
+ * @namespace sofa.UrlConstructionService
+ *
+ * @description
+ * As the name says. This service provides methods to construct URLs for
+ * different use cases.
+ */
+sofa.define('sofa.UrlConstructionService', function (configService) {
+    var self = {};
+
+    /**
+     * @method createUrlForProducts
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for products.
+     *
+     * @param {int} categoryUrlId Category url id.
+     * @return {string} Url
+     */
+    self.createUrlForProducts = function (categoryUrlId) {
+        return '/cat/' + categoryUrlId + '/products';
+    };
+
+    /**
+     * @method createUrlForProduct
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for a product.
+     *
+     * @param {product} product Product object.
+     * @return {string} Url
+     */
+    self.createUrlForProduct = function (product) {
+        return '/cat/' + product.categoryUrlId + '/product/' + product.urlKey;
+    };
+
+    /**
+     * @method createUrlForCategory
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for a category.
+     *
+     * @param {int} categoryUrlId Category url id.
+     * @return {string} Url
+     */
+    self.createUrlForCategory = function (categoryUrlId) {
+        return '/cat/' + categoryUrlId;
+    };
+
+    /**
+     * @method createUrlForRootCategory
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for root category.
+     *
+     * @return {string} Url
+     */
+    self.createUrlForRootCategory = function () {
+        return '';
+    };
+
+    /**
+     * @method createUrlForCart
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for cart.
+     *
+     * @return {string} Url
+     */
+    self.createUrlForCart = function () {
+        return '/cart';
+    };
+
+    /**
+     * @method createUrlForCheckout
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for checkout.
+     *
+     * @return {string} Url
+     */
+    self.createUrlForCheckout = function () {
+        return '/checkout';
+    };
+
+    /**
+     * @method createUrlForSummary
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for summary.
+     *
+     * @param {string} token Summary token.
+     * @return {string} Url
+     */
+    self.createUrlForSummary = function (token) {
+        return '/summary/' + token;
+    };
+
+    /**
+     * @method createUrlForShippingCostsPage
+     * @memberof sofa.UrlConstructionService
+     *
+     * @description
+     * Creates url for shipping costs page.
+     *
+     * @return {string} Url
+     */
+    self.createUrlForShippingCostsPage = function () {
+        return '/pages/' + configService.get('linkShippingCosts', '');
+    };
+
+    return self;
+});
+
+} (sofa));
---
+++ test/sofa.urlConstructionService.spec.js
@@ -0,0 +1,177 @@
+'use strict';
+/* global sofa */
+
+describe('sofa.UrlConstructionService', function () {
+
+    var urlConstructionService;
+
+    beforeEach(function () {
+        var configService = new sofa.ConfigService();
+        urlConstructionService = new sofa.UrlConstructionService(configService);
+    });
+
+    it('should be defined', function () {
+        expect(urlConstructionService).toBeDefined();
+    });
+
+    it('should have a method createUrlForProducts', function () {
+        expect(urlConstructionService.createUrlForProducts).toBeDefined();
+    });
+
+    it('should have a method createUrlForProduct', function () {
+        expect(urlConstructionService.createUrlForProduct).toBeDefined();
+    });
+
+    it('should have a method createUrlForCategory', function () {
+        expect(urlConstructionService.createUrlForCategory).toBeDefined();
+    });
+
+    it('should have a method createUrlForRootCategory', function () {
+        expect(urlConstructionService.createUrlForRootCategory).toBeDefined();
+    });
+
+    it('should have a method createUrlForCart', function () {
+        expect(urlConstructionService.createUrlForCart).toBeDefined();
+    });
+
+    it('should have a method createUrlForCheckout', function () {
+        expect(urlConstructionService.createUrlForCheckout).toBeDefined();
+    });
+
+    it('should have a method createUrlForSummary', function () {
+        expect(urlConstructionService.createUrlForSummary).toBeDefined();
+    });
+
+    it('should have a method createUrlForShippingCostsPage', function () {
+        expect(urlConstructionService.createUrlForShippingCostsPage).toBeDefined();
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForProducts', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForProducts).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForProducts()).toBe('string');
+        });
+
+        it('should return url for products', function () {
+            expect(urlConstructionService.createUrlForProducts('foo')).toEqual('/cat/foo/products');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForProduct', function () {
+
+        var product;
+
+        beforeEach(function () {
+            product = {
+                categoryUrlId: 'foo',
+                urlKey: 'bar'
+            };
+        });
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForProduct).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForProduct(product)).toBe('string');
+        });
+
+        it('should return url for product', function () {
+            expect(urlConstructionService.createUrlForProduct(product)).toEqual('/cat/foo/product/bar');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForCategory', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForCategory).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForCategory()).toBe('string');
+        });
+
+        it('should return url for category', function () {
+            expect(urlConstructionService.createUrlForCategory('foo')).toEqual('/cat/foo');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForRootCategory', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForRootCategory).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForRootCategory()).toBe('string');
+        });
+
+        it('should return url for roo category', function () {
+            expect(urlConstructionService.createUrlForRootCategory()).toEqual('');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForCart', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForCart).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForCart()).toBe('string');
+        });
+
+        it('should return url for cart', function () {
+            expect(urlConstructionService.createUrlForCart()).toEqual('/cart');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForCheckout', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForCheckout).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForCheckout()).toBe('string');
+        });
+
+        it('should return url for checkout', function () {
+            expect(urlConstructionService.createUrlForCheckout()).toEqual('/checkout');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForSummary', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForSummary).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForSummary()).toBe('string');
+        });
+
+        it('should return url for summary', function () {
+            expect(urlConstructionService.createUrlForSummary('foo')).toEqual('/summary/foo');
+        });
+    });
+
+    describe('sofa.UrlConstructionService#createUrlForShippingCostsPage', function () {
+
+        it('should be a function', function () {
+            expect(typeof urlConstructionService.createUrlForShippingCostsPage).toBe('function');
+        });
+
+        it('should return a string', function () {
+            expect(typeof urlConstructionService.createUrlForShippingCostsPage()).toBe('string');
+        });
+
+        it('should return url for shipping costs', function () {
+            expect(urlConstructionService.createUrlForShippingCostsPage()).toEqual('/pages/');
+        });
+    });
+});
