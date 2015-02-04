c699be9c0f26c488da396edf5de8cf8b598baf05
Pascal Precht <pascal.precht@googlemail.com>
2015-02-04 17:45:12 +0100

    chore(dep): updates build process and generates new files

---
+++ dist/sofaUrlConstructionService.angular.js
@@ -0,0 +1,17 @@
+/**
+ * sofa-url-construction-service - v0.5.0 - Wed Feb 04 2015 17:43:50 GMT+0100 (CET)
+ * http://www.sofa.io
+ *
+ * Copyright (c) 2014 CouchCommerce GmbH (http://www.couchcommerce.com / http://www.sofa.io) and other contributors
+ * THIS SOFTWARE CONTAINS COMPONENTS OF THE SOFA.IO COUCHCOMMERCE SDK (WWW.SOFA.IO)
+ * IT IS PROVIDED UNDER THE LICENSE TERMS OF THE ATTACHED LICENSE.TXT.
+ */
+;(function (angular) {
+'use strict';
+
+angular.module('sofa.urlConstructionService', ['sofa.core'])
+
+.factory('urlConstructionService', ["configService", function (configService) {
+    return new sofa.UrlConstructionService(configService);
+}]);
+}(angular));
